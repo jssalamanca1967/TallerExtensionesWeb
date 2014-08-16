c5b027c8eff2ff674828529f8320bbe908c15cf7
jssalamanca1967 <js.salamanca1967@uniandes.edu.co>
2014-08-15 19:47:43 -0500

    Member model con las modificaciones

--- sportclub.web/src/main/webapp/src/model/memberModel.js
+++ sportclub.web/src/main/webapp/src/model/memberModel.js
@@ -32,14 +32,96 @@ define(['model/_memberModel'], function() {
 
  	validate: function(attrs,options){
             var validationMessage = "";
-            if(!attrs.name){
-                validationMessage = "The name can't be empty.";
+            if (!attrs.name || !attrs.firsName || !attrs.lastName || !attrs.birthDate || !attrs.enable || !attrs.docNumber|| !attrs.documenttypeId  || !attrs.partnerId )
+            {
+                validationMessage = "Ninguno de los campos puede estar vacio";
+            }
+            function compara_fecha(fecha)
+            {
+                //Retorna true si la fecha ingresada es mayor o igual a la actual
+                
+                
+                var fechaActual = new Date()
+                var diaActual = fechaActual.getDate();
+                var mmActual = fechaActual.getMonth() + 1;
+                var yyyyActual = fechaActual.getFullYear();
+                var FechaNac = fecha.split("/");
+                var diaCumple = FechaNac[0];
+                var mmCumple = FechaNac[1];
+                var yyyyCumple = FechaNac[2];
+               
+                if(yyyyCumple>yyyyActual)
+                {
+                    return true;
+                }
+                else if(yyyyCumple===yyyyActual)
+                {
+                    if(mmCumple>mmActual)
+                    {
+                        return true;
+                    }
+                    else if(mmCumple===mmActual)
+                    {
+                        if(diaCumple>=diaActual)
+                        {
+                            return true;
+                        }
+                    }
+                    
+                }
+                return false;
+            }
+            function calcular_Edad(fecha) {
+                var fechaActual = new Date();
+                var diaActual = fechaActual.getDate();
+                var mmActual = fechaActual.getMonth() + 1;
+                var yyyyActual = fechaActual.getFullYear();
+                var FechaNac = fecha.split("/");
+                var diaCumple = FechaNac[0];
+                var mmCumple = FechaNac[1];
+                var yyyyCumple = FechaNac[2];
+                //retiramos el primer cero de la izquierda
+                if (mmCumple.substr(0,1) == 0) {
+                    mmCumple= mmCumple.substring(1, 2);
+                }
+                //retiramos el primer cero de la izquierda
+                if (diaCumple.substr(0, 1) == 0) {
+                    diaCumple = diaCumple.substring(1, 2);
+                }
+                var edad = yyyyActual - yyyyCumple;
+
+                //validamos si el mes de cumpleaños es menor al actual
+                //o si el mes de cumpleaños es igual al actual
+                //y el dia actual es menor al del nacimiento
+                //De ser asi, se resta un año
+                if ((mmActual < mmCumple) || (mmActual == mmCumple && diaActual < diaCumple)) {
+                edad--;
+                }
+                return edad;
+        }
+            var edad=calcular_Edad(attrs.birthDate);
+            
+            if(edad>100)
+            {
+                validationMessage = "La edad del miembro del club no puede superar los 100 años";
+            }
+            if(attrs.docNumber < 5)
+            {
+                validationMessage = "El número de documento debe tener por lo menos 5 caracteres";                
+            }
+            if(!attrs.documenttypeId)
+            {
+                validationMessage = "Se debe especificar el tipo de documento";
+            }
+            if(compara_fecha(birthDate))
+            {
+                validationMessage = "La fecha de nacimiento del miembro no puede superar ni ser igual a la fecha actual";                                
             }
             if(validationMessage.length>0){
                return validationMessage;
             }
         }
-
+       
     });
 
     App.Model.MemberList = App.Model._MemberList.extend({
